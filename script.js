9cef90368ae1b5b37f58103d6d9e60d93d93d0ea
Slava <kuncovs19@gmail.com>
2023-02-21 13:38:39 +0300

    create slider for cards and add fetch for customers

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
+let dataLength = 0;
+
 async function getData() {
 	try {
-		let response = await fetch("http://localhost:3001/rooms")
+		let response = await fetch('http://localhost:3001/rooms')
 		let data = await response.json()
-		dataOutput(data)
+		console.log(data)
+		cardDataOutput(data)
 	} 
 	catch (err) {
 		console.log(err);
-		roomsCarts.innerHTML = `<h3>No rooms found</h3>`
+		// roomsCarts.innerHTML = `<h3>No rooms found</h3>`
 	}
 }
 getData() 
 
-dataOutput = (data) => {
+cardDataOutput = (data) => {
+	getLength(data)
 	if(data.length === 0) {
 		roomsCarts.innerHTML = `<h3>No rooms found</h3>`
+		leftButton.style.display = "none"
+		rightButton.style.display = "none"
 	}
 	else{
 		for (let key in data) {
-			roomsCarts.innerHTML += `
+			track.innerHTML += `
 				<div class="room-cart">
 					<img src="${data[key].image}" alt="">
 					<div class="cart-text">
@@ -40,6 +46,63 @@ dataOutput = (data) => {
 					</div>
 				</div>
 			`
-	}
+		}
+		
 	}	
 }
+
+getLength = (data) => {
+	dataLength = data.length
+}
+
+let position = 0;
+let itemWidth = 379.99;
+
+leftButton.addEventListener('click', () => {
+	position += itemWidth
+	setPosition()
+})
+
+rightButton.addEventListener('click', () => {
+	position -= itemWidth
+	setPosition()
+})
+
+setPosition = () => {
+	track.style.transform = `translateX(${position}px)`
+	track.style.transition = `transform .3s ease-in-out`
+	checkButtons()
+}
+
+checkButtons = () => {
+	if(position == 0) {
+		leftButton.setAttribute('disabled', true)
+	}
+	else{
+		leftButton.removeAttribute('disabled')
+	}
+	if(position === -((dataLength - 3) * itemWidth)) {
+		rightButton.setAttribute('disabled', true)
+		rightButton.style.background = ''
+	}
+	else{
+		rightButton.removeAttribute('disabled')
+	} 
+}
+checkButtons()
+
+// =================================================================
+
+async function getComment() {
+	try {
+		let response = await fetch('http://localhost:3001/customers')
+		let data = await response.json()
+		console.log(data)
+		cardDataOutput(data)
+	} 
+	catch (err) {
+		console.log(err);
+		// roomsCarts.innerHTML = `<h3>No rooms found</h3>`
+	}
+}
+getComment() 
